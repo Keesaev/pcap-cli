b1d333c0d5cb03a57c0ab7bf05d3aaec2464ab56
keesaev <kostia.234@mail.ru>
2022-10-27 00:10:50 +0300

    Finished tcp deserialization

--- src/protocol-headers/transport/tcp.cpp
+++ src/protocol-headers/transport/tcp.cpp
@@ -2,6 +2,32 @@
 
 #include <iostream> // TODO rm
 
+#include <netinet/in.h> // htons
+
+#include <iomanip> // setw, setfill
+#include <sstream>
+
+const std::array<std::string, tcp::_field_count> tcp::_descriptions {
+    "Source Port",
+    "Destination Port",
+    "Sequence Number (SN)",
+    "Acknowledgment Number (ACK SN)",
+    "Data offset (Header length)",
+    "Reserved",
+    "NS (ECN-nonce)",
+    "CWR (Congestion Window Reduced)",
+    "ECE (ECN-Echo)",
+    "URG",
+    "ACK",
+    "PSH",
+    "RST",
+    "SYN",
+    "FIN",
+    "Window size",
+    "Checksum",
+    "Urgent Point"
+};
+
 tcp::tcp(const unsigned char* bytes)
     : _data { *reinterpret_cast<const tcp_h*>(bytes) }
 {
@@ -9,10 +35,57 @@ tcp::tcp(const unsigned char* bytes)
 
 std::string tcp::hex() const
 {
-    return "";
+    auto bytes = reinterpret_cast<const uint8_t*>(&_data);
+    std::stringstream stream;
+    for (int i = 0; i < sizeof(tcp); i++) {
+        stream << std::setw(2) << std::setfill('0') << std::hex << (int)bytes[i] << ' ';
+    }
+    return stream.str();
 }
 
 const std::pair<std::string, std::string> tcp::operator[](std::size_t idx) const
 {
-    return { "", "" };
+    auto value = [&data = _data](std::size_t idx) -> std::string {
+        switch (idx) {
+        case 0:
+            return std::to_string(::htons(data.src_port));
+        case 1:
+            return std::to_string(::htons(data.dst_port));
+        case 2:
+            return std::to_string(::htonl(data.seq_num));
+        case 3:
+            return std::to_string(::htonl(data.ack_num));
+        case 4:
+            return std::to_string(data.control.data_offset);
+        case 5:
+            return std::to_string(data.control.reserved);
+        case 6:
+            return std::to_string(data.control.ns);
+        case 7:
+            return std::to_string(data.control.cwr);
+        case 8:
+            return std::to_string(data.control.ece);
+        case 9:
+            return std::to_string(data.control.urg);
+        case 10:
+            return std::to_string(data.control.ack);
+        case 11:
+            return std::to_string(data.control.psh);
+        case 12:
+            return std::to_string(data.control.rst);
+        case 13:
+            return std::to_string(data.control.syn);
+        case 14:
+            return std::to_string(data.control.fin);
+        case 15:
+            return std::to_string(::htons(data.win_size));
+        case 16:
+            return std::to_string(::htons(data.checksum));
+        case 17:
+            return std::to_string(::htons(data.urg_ptr));
+        default:
+            return "";
+        }
+    };
+    return { value(idx), _descriptions[idx] };
 }
--- src/protocol-headers/transport/tcp.h
+++ src/protocol-headers/transport/tcp.h
@@ -2,22 +2,23 @@
 
 #include "transport.hpp"
 
+#include <array>
 #include <stdint.h>
 
 class tcp final : public transport {
 #pragma pack(push, 1)
     struct tcp_control {
-        uint8_t data_offset : 4;
-        uint8_t reserved : 3;
         uint8_t ns : 1;
-        uint8_t cwr : 1;
-        uint8_t ece : 1;
-        uint8_t urg : 1;
-        uint8_t ack : 1;
-        uint8_t psh : 1;
-        uint8_t rst : 1;
-        uint8_t syn : 1;
+        uint8_t reserved : 3;
+        uint8_t data_offset : 4;
         uint8_t fin : 1;
+        uint8_t syn : 1;
+        uint8_t rst : 1;
+        uint8_t psh : 1;
+        uint8_t ack : 1;
+        uint8_t urg : 1;
+        uint8_t ece : 1;
+        uint8_t cwr : 1;
     };
     struct tcp_h {
         uint16_t src_port;
@@ -30,13 +31,15 @@ class tcp final : public transport {
         uint16_t urg_ptr;
     } _data;
 #pragma pack(pop)
+    static const int _field_count = 18;
+    static const std::array<std::string, _field_count> _descriptions;
 
 public:
     tcp(const unsigned char* bytes);
     virtual std::string name() const final { return "TCP"; }
     virtual std::string hex() const final;
     virtual int header_size() const final { return sizeof(tcp_h); }
-    virtual int field_count() const final { return 0; }
+    virtual int field_count() const final { return _field_count; }
     virtual const std::pair<std::string, std::string> operator[](std::size_t idx) const final;
 
     virtual ~tcp() { }
